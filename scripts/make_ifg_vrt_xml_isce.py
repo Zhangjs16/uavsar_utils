027ac9fe8095873036e47b293d51f72099a30832
Talib Oliver <taliboliver88@gmail.com>
2023-12-08 11:04:00 -0800

    create xml and vrt using isce
    Script to generate UAVSAR ifgs xmls and vrts using isce.

---
+++ scripts/make_ifg_vrt_xml_isce.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+############################################################
+# Script to generate UAVSAR ifgs xmls and vrts using isce. #
+# Author: Talib Oliver, 2021                               #
+############################################################
+
+import os
+import glob
+import argparse
+import json
+import isce
+import isceobj
+from isceobj.Util.decorators import use_api
+from iscesys.ImageUtil.ImageUtil import ImageUtil as IU
+
+def createParser():
+    '''
+    Create command line parser.
+    '''
+
+    parser = argparse.ArgumentParser(description='Script to generate UAVSAR ifgs xmls and vrts using isce: int | amp | cor | unw | conncomp')
+    parser.add_argument('-i', '--input', dest='ifgdir', type=str, required=True,
+            help='Ifgram directory.')
+    parser.add_argument('-s', '--samples', dest='samples', type=int, required=True,
+            help='Image sample number. Found as Slant Range Data Range Samples in UAVSAR .ann file')
+    return parser
+
+
+def cmdLineParse(iargs=None):
+    '''
+    Command line parser.
+    '''
+
+    parser = createParser()
+    return parser.parse_args(args = iargs)
+
+
+def run_ifg(int_file, samples):
+    outInt = isceobj.Image.createIntImage()
+    outInt.setFilename(int_file)
+    outInt.setWidth(samples)
+    outInt.setAccessMode('read')
+    outInt.renderHdr()
+    outInt.renderVRT()
+
+def run_unw(unw_file, samples):
+    outCor = isceobj.Image.createImage()
+    outCor.setFilename(unw_file)
+    outCor.setWidth(samples)
+    outCor.setAccessMode('read')
+    outCor.setDataType('FLOAT')
+    outCor.renderHdr()
+    outCor.renderVRT()
+
+def run_amp(amp_file, samples):
+    outAmp = isceobj.Image.createAmpImage()
+    outAmp.setFilename(amp_file)
+    outAmp.setWidth(samples)
+    outAmp.setAccessMode('read')
+    outAmp.renderHdr()
+    outAmp.renderVRT()
+
+def run_cor(cor_file, samples):
+    outCor = isceobj.Image.createImage()
+    outCor.setFilename(cor_file)
+    outCor.setWidth(samples)
+    outCor.setAccessMode('read')
+    outCor.setDataType('FLOAT')
+    outCor.renderHdr()
+    outCor.renderVRT()
+
+def run_cc(cc_file, samples):
+    outCc = isceobj.Image.createImage()
+    outCc.setFilename(cc_file)
+    outCc.setWidth(samples)
+    outCc.setAccessMode('read')
+    outCc.setDataType('BYTE') # BYTE | FLOAT | UINT
+    outCc.renderHdr()
+    outCc.renderVRT()
+
+
+def main(iargs=None):
+
+    inps = cmdLineParse(iargs)  
+    ifgs = glob.glob(inps.ifgdir+'/*')
+    for img in ifgs:
+        ftype = img.split('.')[1]
+        if ftype == 'int': 
+            run_ifg(img, inps.samples)
+        elif ftype == 'amp':
+            run_amp(img, inps.samples)
+        elif ftype == 'cor':
+            run_cor(img, inps.samples)
+        elif ftype == 'unw':
+            run_unw(img, inps.samples)
+        elif ftype == 'conncomp':
+            run_cc(img, inps.samples)
+    print('This is the end')
+
+if __name__ == '__main__':
+
+    main() 
+ 
+    '''
+    Main driver.
+    '''
